89fbf9e2406887945f9331892257e971ddabb1c1
bala1996 <balaokate@gmail.com>
2017-07-14 11:40:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,57 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one':{
+		title:'Article One',
+		heading:'Article One',
+		date:'12/6/17',
+		content:'<p>My first article<p>'
+	}
+
+};
+
+
+function createTemplate(data)
+{
+	//var title = data.title;
+	//console.log(title);
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate =
+	`<html>
+		<head>
+			<title>Article One</title>
+		</head>
+		<body>
+			<div>
+				<h3>${heading}</h3>
+			</div>
+			<div>
+				${date}
+			</div>
+			<div>
+				${content}
+			</div>
+		</body>
+	</html>`;
+
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/:articleName', function(req,res){
+	var articleName = req.param.articleName;
+	res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {
